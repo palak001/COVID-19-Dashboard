94f20244d6d7bc97ff56b8e6e5d4c00bef8f8cf2
Palak <palak7372@gmail.com>
2020-04-16 16:48:14 +0530

    source data updated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 d3.queue()
-    .defer(d3.json, "worldmap.json")
-    .defer(d3.json, "worldCases.json")
+    .defer(d3.json, "https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json")
+    .defer(d3.json, "https://api.covid19api.com/summary")
     .defer(d3.json, "https://pomber.github.io/covid19/timeseries.json")
     .await(function(error, mapData, caseData, countryData) {
         if(error) throw error;
@@ -14,7 +14,7 @@ d3.queue()
         let height = 300;
         createWorldMap(width, width*4 / 5);
         createBar(width, height);
-        drawWorldMap(geoData, caseData, countryData);
+        drawWorldMap(geoData, caseData.Countries, countryData);
         drawBar(countryData, "", ""); //country name
         
     })
