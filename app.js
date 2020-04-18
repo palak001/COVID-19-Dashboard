52f764707692f6f68520908a8fc76264708cb226
Palak <palak7372@gmail.com>
2020-04-18 22:09:58 +0530

    added search bar

--- app.js
+++ app.js
@@ -7,6 +7,28 @@ d3.queue()
 
         let worldGeoData = topojson.feature(worldMapData, worldMapData.objects.countries).features;
 
+        //list of all country names
+        let ul = document.getElementById("countryList");
+        for(let i = 0; i < worldCaseData.Countries.length; i++) {
+            let countryName = worldCaseData.Countries[i].Country;
+            let countryCode = worldCaseData.Countries[i].CountryCode;
+            let li = document.createElement("li");
+            li.innerHTML = `${countryName}(${countryCode})`;
+            ul.appendChild(li);
+        }
+        // d3.select("#searchBar")
+        //     .on("keydown", searchFunc);
+
+        let searchBar = document.getElementById("searchBar");
+        searchBar.addEventListener("keyup", searchFunc);
+        searchBar.onblur = function() {
+            document.getElementById("controlHeight").style.display = "none";
+        }
+        searchBar.onfocus = function() {
+            document.getElementById("controlHeight").style.display = "inline";
+        }
+        // console.log(countryName);
+
             createWorldMap();
             createBar("confirmed");
             createBar("death")
@@ -15,6 +37,26 @@ d3.queue()
             drawBar(countryDailyData, "", "", "confirmed");
             drawBar(countryDailyData, "", "", "death");
             drawBar(countryDailyData, "", "", "recovered");
+
+        function searchFunc() {
+            let input = document.getElementById("searchBar");
+            let inputText = input.value.toLowerCase();      // so far entered search term
+            let li = document.getElementsByTagName("li"); // array of list tags
+            let ul = document.getElementById("controlHeight");
+            ul.style.display = "";
+            let liText;
+            for(let i = 0; i < li.length; i++) {
+                liText = li[i].innerText;
+                // console.log(liText);
+                if(liText.toLowerCase().indexOf(inputText) > -1) {
+                    li[i].style.display = "";
+                }
+                else {
+                    li[i].style.display = "none";
+                }
+            }
+
+        }
 
     
     })
